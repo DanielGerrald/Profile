3bf7cf4e96ef8f0971bd49061212a2059c61fa7a
Daniel Gerrald <daniel.gerrald@icloud.com>
2020-10-29 17:41:43 -0400

    update about be section

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -18,20 +18,21 @@ class About extends Component {
                 My name is Daniel Gerrald. I am a full stack web developer as
                 well as an accomplished Network Administrator with a
                 demonstrated history working in the government administration
-                industry. Strong information technology professional and recent
-                graduate from the Coding: Full Stack Development certificate
-                program from the University of North Carolina Charlotte and
-                building upon my knowledge and experience within the field. In
-                this intensive program I curated aptitudes in JavaScript, CSS,
-                React.js, and responsive web design. I am energetic about
-                approaching programming challenges from different perspectives
-                and partnering with others to create meaningful web
-                applications. My background includes an Associate's degree
-                centered in Computer Systems Networking and Telecommunications
-                from Florence-Darlington Technical College and over nine years
-                of information technology experience. I am driven by computer
-                science fundamentals and appreciate breaking down a problem
-                piece-by-piece to find a solution.
+                industry. I am a strong information technology professional and
+                recent graduate from the Coding: Full Stack Development
+                certificate program from the University of North Carolina
+                Charlotte. In this intensive program I curated aptitudes in
+                JavaScript, CSS, React.js, and responsive web design. I am always
+                looking to build upon my knowledge and experience within the
+                technology field. I am energetic about approaching programming
+                challenges from different perspectives and partnering with
+                others to create meaningful web applications. My background
+                includes an Associate's degree centered in Computer Systems
+                Networking and Telecommunications from Florence-Darlington
+                Technical College and over nine years of information technology
+                experience. I am driven by computer science fundamentals and
+                appreciate breaking down a problem piece-by-piece to find a
+                solution.
               </p>
             </div>
           </div>
