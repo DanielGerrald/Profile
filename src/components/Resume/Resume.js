4f02494c05dfa28f34d9728e5c2dc3a0e4de0e76
Daniel Gerrald <daniel.gerrald@icloud.com>
2021-03-24 16:09:32 -0400

    Update Resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import CV from "../../inc/Daniel Gerrald - Resume 2020.pdf";
+import CV from "../../inc/Daniel Gerrald - Resume.pdf";
 import { Link } from "react-router-dom";
 
 class Resume extends Component {
@@ -28,6 +28,40 @@ class Resume extends Component {
                 </div>
 
                 <div className="timeline-header">
+                  <h3>UNC Charlotte / Trilogy Bootcamp</h3>
+                  <p>August 2020 - October 2020</p>
+                </div>
+
+                <div className="timeline-content">
+                  <h4>Coding Bootcamp Certification</h4>
+                  <p>
+                    Computer Science applied to JavaScript, Databases (MySQL,
+                    MongoDB), Server Side Development (MERN Stack, Progressive
+                    Web Applications), Browser Based Technologies (HTML5, CSS,
+                    JavaScript, jQuery, Bootstrap), Deployment/Command-Line
+                    Fundamentals (Heroku, Git), API Interaction (API, JSON,
+                    AJAX), Quality Assurance (Unit Testing, Linting, Continuous
+                    Integration), Agile Methodology
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row resume-timeline">
+          <div className="col-twelve resume-header">
+            <h2>Work Experience</h2>
+          </div>
+
+          <div className="col-twelve">
+            <div className="timeline-wrap">
+              <div className="timeline-block">
+                <div className="timeline-ico">
+                  <i className="fa fa-briefcase"></i>
+                </div>
+
+                <div className="timeline-header">
                   <h3>Avast Antivirus</h3>
                   <p>February 2019 - July 2020</p>
                 </div>
@@ -37,7 +71,21 @@ class Resume extends Component {
                   <p>
                     Managed and implemented support processes and best practices
                     for new software product offering – Avast Secure Internet
-                    Gateway (SIG) built on ZScaler technology.
+                    Gateway (SIG) built on ZScaler technology. Participated in
+                    design of call/chat/email monitoring formats and quality
+                    standards. Owned, developed and implemented QA processes in
+                    customer care. Ensured support team provided the highest
+                    quality of support by (but not limited to) Conducting QA
+                    reviews and monitoring adherence to the QA process across
+                    all channels (phones, chats, tickets). Identified and
+                    implemented improvements based on QA results for increased
+                    customer satisfaction. Provided one-on-one mentoring to new
+                    employees or those having difficulty meeting quality
+                    requirements. Developed and implemented training programs
+                    for both internal agents as well as for external customers.
+                    Reviewed and interpreted Customer Care quality assurance
+                    data to identify trends and develop recommendations to
+                    improve customer experience and operational efficiency.
                   </p>
                 </div>
               </div>
@@ -55,8 +103,28 @@ class Resume extends Component {
                 <div className="timeline-content">
                   <h4>Network Administrator</h4>
                   <p>
-                    Managed and maintained network security and reliability
-                    across multiple departments and facilities.
+                    Managed 200 - 300 users and end devices, and 30 servers both
+                    physical and virtual. Maintained emergency services such as
+                    911 dispatch, fire department and police department on
+                    99.999% uptime basis. Migrated domain controllers from
+                    Windows 2008 to 2016 servers. Upgraded VMware environment
+                    from 5.5 to 6.5. Implemented disaster recovery site using
+                    vmware replication. Implemented Meraki wireless networks and
+                    devices. Upgraded SCCM 2012 to SCCM Current Branch, and
+                    managed software deployment and windows updates. Secured
+                    network by implementing vlan segregated networks; maintained
+                    documentation. Maintained and enforced network security
+                    standards according to South Carolina Law Enforcement
+                    Division (SLED) and Criminal Justice Information Service
+                    (CJIS) compliance. Planned new network configurations and
+                    integration into existing networks to maintain optimal
+                    performance and replaced network switches and routers with
+                    new models. Managed IP telephone system Shoretel/Mitel.
+                    Evaluated business needs, objectives and goals, researching
+                    products available and designating procedures to best meet
+                    the needs of the city. Maintained proper user access control
+                    with shared files and data. Managed cloud services and
+                    subscriptions.
                   </p>
                 </div>
               </div>
@@ -144,7 +212,13 @@ class Resume extends Component {
             >
               Portfolio
             </Link>
-            <a target="_blank" rel="noopener noreferrer" href={CV} title="Download CV" className="button button-primary">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={CV}
+              title="Download CV"
+              className="button button-primary"
+            >
               Download CV
             </a>
           </div>
