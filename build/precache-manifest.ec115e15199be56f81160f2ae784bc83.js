183d41888420b1237019e86373bf4373b983e2ee
Daniel Gerrald <daniel.gerrald@icloud.com>
2024-01-04 19:27:56 +0000

    open ssl legacy provider edit in package.json

---
+++ build/precache-manifest.ec115e15199be56f81160f2ae784bc83.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3386756cbe023eaa1175ea10cad7e98e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2894039508ea87752f19",
+    "url": "/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "224ba1a5c5c601200211",
+    "url": "/static/css/main.2fdcf9bd.chunk.css"
+  },
+  {
+    "revision": "2894039508ea87752f19",
+    "url": "/static/js/2.819952e1.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.819952e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224ba1a5c5c601200211",
+    "url": "/static/js/main.25201c92.chunk.js"
+  },
+  {
+    "revision": "ea2c8fd09ea148de3b2a",
+    "url": "/static/js/runtime-main.2ea34f28.js"
+  },
+  {
+    "revision": "16f4aaef3362ed7b0fda3d1188eacd0e",
+    "url": "/static/media/Daniel Gerrald - Resume.16f4aaef.pdf"
+  },
+  {
+    "revision": "bad88d45f048e8d46f09359fc69cd1ac",
+    "url": "/static/media/bg.bad88d45.webp"
+  },
+  {
+    "revision": "65bcbc899f379216109acd0b6c494618",
+    "url": "/static/media/fontawesome-webfont.65bcbc89.svg"
+  },
+  {
+    "revision": "7149833697a959306ec3012a8588dcfa",
+    "url": "/static/media/fontawesome-webfont.71498336.eot"
+  },
+  {
+    "revision": "c4668ed2440df82d3fd2f8be9d31d07d",
+    "url": "/static/media/fontawesome-webfont.c4668ed2.ttf"
+  },
+  {
+    "revision": "d95d6f5d5ab7cfefd09651800b69bd54",
+    "url": "/static/media/fontawesome-webfont.d95d6f5d.woff"
+  },
+  {
+    "revision": "3c815a4b0627cbacd15a7a09c0e2b695",
+    "url": "/static/media/icomoon.3c815a4b.woff"
+  },
+  {
+    "revision": "59809af617d58f5da50fb027f3c617ef",
+    "url": "/static/media/icomoon.59809af6.ttf"
+  },
+  {
+    "revision": "81edc20628720a70eec4bb593dc06041",
+    "url": "/static/media/icomoon.81edc206.svg"
+  },
+  {
+    "revision": "8ba3d2f45fb8fb79875871d14df2c703",
+    "url": "/static/media/icomoon.8ba3d2f4.eot"
+  },
+  {
+    "revision": "641a9a6253c5177af658f35511693070",
+    "url": "/static/media/intro-bg.641a9a62.webp"
+  },
+  {
+    "revision": "711986fd9c81541c4b6d99db52e77acb",
+    "url": "/static/media/logo.711986fd.webp"
+  },
+  {
+    "revision": "980daf324e301d456acc1251460a1bc7",
+    "url": "/static/media/lora-bold-webfont.980daf32.svg"
+  },
+  {
+    "revision": "be4c6e2b6409b6c937f9e49de13adb8d",
+    "url": "/static/media/lora-bold-webfont.be4c6e2b.woff"
+  },
+  {
+    "revision": "e5a440c712e2056147c0fbc5f9360a53",
+    "url": "/static/media/lora-bold-webfont.e5a440c7.eot"
+  },
+  {
+    "revision": "f8ac2784360799ba55493153ab7f60c6",
+    "url": "/static/media/lora-bold-webfont.f8ac2784.ttf"
+  },
+  {
+    "revision": "155fed8a12566558b5c3742bf48cbf23",
+    "url": "/static/media/lora-bolditalic-webfont.155fed8a.eot"
+  },
+  {
+    "revision": "63f6ee0f600a54684190896e8d83f9de",
+    "url": "/static/media/lora-bolditalic-webfont.63f6ee0f.ttf"
+  },
+  {
+    "revision": "a1b0823d2c202c4dfccea7bd9d00eb00",
+    "url": "/static/media/lora-bolditalic-webfont.a1b0823d.woff"
+  },
+  {
+    "revision": "e30d63912d9a716b75b40903a22bad7e",
+    "url": "/static/media/lora-bolditalic-webfont.e30d6391.svg"
+  },
+  {
+    "revision": "51f319fe6f9569843f9eece7f743ba14",
+    "url": "/static/media/lora-italic-webfont.51f319fe.svg"
+  },
+  {
+    "revision": "5840a3fc3fc7f1cf9622e04b0f4292fc",
+    "url": "/static/media/lora-italic-webfont.5840a3fc.eot"
+  },
+  {
+    "revision": "980aae3a10d1e193ff527123af76898d",
+    "url": "/static/media/lora-italic-webfont.980aae3a.ttf"
+  },
+  {
+    "revision": "98a9fc27d576ea195ead28f8a9d4ffa1",
+    "url": "/static/media/lora-italic-webfont.98a9fc27.woff"
+  },
+  {
+    "revision": "65873b44e2940ed66d5a8b12e5597615",
+    "url": "/static/media/lora-regular-webfont.65873b44.svg"
+  },
+  {
+    "revision": "8541664ae124b8e5df7d1af7ff907c7b",
+    "url": "/static/media/lora-regular-webfont.8541664a.eot"
+  },
+  {
+    "revision": "970ef6b8c13b9c0b50dc343c15f1576d",
+    "url": "/static/media/lora-regular-webfont.970ef6b8.woff"
+  },
+  {
+    "revision": "cf6207b3420b4c3643429addddb78eaa",
+    "url": "/static/media/lora-regular-webfont.cf6207b3.ttf"
+  },
+  {
+    "revision": "13e9632bec05847de11bebb9de5ef1cf",
+    "url": "/static/media/poppins-bold-webfont.13e9632b.woff2"
+  },
+  {
+    "revision": "47a731ab9f21cd153d181fad8dd76af6",
+    "url": "/static/media/poppins-bold-webfont.47a731ab.ttf"
+  },
+  {
+    "revision": "794eab2be996e11c11bc539525c9f11b",
+    "url": "/static/media/poppins-bold-webfont.794eab2b.svg"
+  },
+  {
+    "revision": "aa98aff4d8d942f77b6dbbbcf5a5bc46",
+    "url": "/static/media/poppins-bold-webfont.aa98aff4.eot"
+  },
+  {
+    "revision": "c7422667d745ba2ce731f4765c15bb49",
+    "url": "/static/media/poppins-bold-webfont.c7422667.woff"
+  },
+  {
+    "revision": "07ed5067561480f15fbd65bafab7a2f4",
+    "url": "/static/media/poppins-light-webfont.07ed5067.woff2"
+  },
+  {
+    "revision": "4100316d96394f75a9797bd00eda2450",
+    "url": "/static/media/poppins-light-webfont.4100316d.eot"
+  },
+  {
+    "revision": "41b61885be97a51f49f2da5ddde74a80",
+    "url": "/static/media/poppins-light-webfont.41b61885.ttf"
+  },
+  {
+    "revision": "44967092131b2de5a9983c76372f0933",
+    "url": "/static/media/poppins-light-webfont.44967092.woff"
+  },
+  {
+    "revision": "709be76c3a24a3ecbaa7f46946a797c4",
+    "url": "/static/media/poppins-light-webfont.709be76c.svg"
+  },
+  {
+    "revision": "4ee1e1689a0d398066672356c35a3f4e",
+    "url": "/static/media/poppins-medium-webfont.4ee1e168.ttf"
+  },
+  {
+    "revision": "954f16b87b54ed7c7d526c39625c459c",
+    "url": "/static/media/poppins-medium-webfont.954f16b8.woff2"
+  },
+  {
+    "revision": "c4814d6b70494e9f7282cd9ac4b41859",
+    "url": "/static/media/poppins-medium-webfont.c4814d6b.svg"
+  },
+  {
+    "revision": "f42126601d14d9bd1eab8b63196d07cc",
+    "url": "/static/media/poppins-medium-webfont.f4212660.woff"
+  },
+  {
+    "revision": "faf3fb02dbbf32176b67986efe042c24",
+    "url": "/static/media/poppins-medium-webfont.faf3fb02.eot"
+  },
+  {
+    "revision": "3509cfd5819b8cf878a79ab52dc9a01b",
+    "url": "/static/media/poppins-regular-webfont.3509cfd5.svg"
+  },
+  {
+    "revision": "42a25c26425cbb63724bc85017b14e1b",
+    "url": "/static/media/poppins-regular-webfont.42a25c26.woff"
+  },
+  {
+    "revision": "8112c2ad6ff2535b458a2ae5fac8e644",
+    "url": "/static/media/poppins-regular-webfont.8112c2ad.eot"
+  },
+  {
+    "revision": "83398fdec6508e339feb6aa038929418",
+    "url": "/static/media/poppins-regular-webfont.83398fde.ttf"
+  },
+  {
+    "revision": "cd8ad1522c0410b8ceda219c25c59308",
+    "url": "/static/media/poppins-regular-webfont.cd8ad152.woff2"
+  },
+  {
+    "revision": "1b69b94bbc3cf9ab506cfe3a1a15acdf",
+    "url": "/static/media/poppins-semibold-webfont.1b69b94b.ttf"
+  },
+  {
+    "revision": "364d921a8c69f5373311b38ca6e976d8",
+    "url": "/static/media/poppins-semibold-webfont.364d921a.woff"
+  },
+  {
+    "revision": "a43dfb376c5abe417448ad4699d9c65d",
+    "url": "/static/media/poppins-semibold-webfont.a43dfb37.eot"
+  },
+  {
+    "revision": "a55f17e069c24c24f1b97dd99f98bf79",
+    "url": "/static/media/poppins-semibold-webfont.a55f17e0.svg"
+  },
+  {
+    "revision": "f98c932e843c0cd0fc3cf41a7fab1a3e",
+    "url": "/static/media/poppins-semibold-webfont.f98c932e.woff2"
+  },
+  {
+    "revision": "10b7b6321f79a9e480d7520a27ddf07e",
+    "url": "/static/media/profile-pic.10b7b632.webp"
+  }
+]);
